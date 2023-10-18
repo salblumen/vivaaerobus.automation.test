1dcf386a58b9b0262c08a0cd26c4d0d5394a10d7
Sal Blumen <uribenski@gmail.com>
2023-10-18 16:34:49 -0600

    Adding POM dependencies and creating BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,34 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+@Slf4j
+@Getter
 public class BaseTest {
+    private WebDriver driver;
+
+@BeforeMethod
+public void beforeMethod(){
+    log.info("------------ Start Test {} --------------");
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+
+    driver.manage().window().maximize();
+
+}
+
+@AfterMethod
+public void AfterMethod(){
+    getDriver().quit();
+}
+
+public void navigateTo(String _url){
+    String url = String.format("http://%s",_url);
+    getDriver().get(url);
+}
+
 }
