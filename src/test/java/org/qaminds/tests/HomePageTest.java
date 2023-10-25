837e87c6e25900feb64c4c2ceeb52fc3856ba231
Sal Blumen <uribenski@gmail.com>
2023-10-24 23:58:41 -0600

    added basic Test suite xml file

--- src/test/java/org/qaminds/tests/HomePageTest.java
+++ src/test/java/org/qaminds/tests/HomePageTest.java
@@ -1,3 +1,5 @@
+package org.qaminds.tests;
+
 import lombok.extern.slf4j.Slf4j;
 import org.qaminds.Pages;
 import org.testng.annotations.Test;
@@ -7,18 +9,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @Slf4j
-public class HomePageTest extends BaseTest{
+public class HomePageTest extends BaseTest {
 
     private String baseUrl = "vivaaerobus.com";
 
-    @Test
+    @Test(testName = "TC-1001 ValidateHomePageLoads", priority = 1, timeOut = 5000)
     public void ValidateHomePageLoads(){
-        log.info("Navigate to vivaaerobus.com");
+        log.info("TC-1001 ValidateHomePageLoads");
+        log.info(" Step 1: Navigate to vivaaerobus.com");
         navigateTo(baseUrl);
         String actualTitle = "Viva Aerobus";
         assertThat(getDriver().getTitle()).contains(actualTitle);
 //        Screenshot.screenshot(getDriver());
         log.info(" Test 1001 ----- SUCCESSFUL -------");
+
+    }
+
+    @Test(testName = "TC-1002 ValidateValidInputsOriginDestinationWorks", priority = 1, enabled = false)
+    public void ValidateValidInputsOriginDestinationWorks(){
+        log.info("TC-1002 ValidateValidInputsOriginDestinationWorks");
+        log.info("Navigate to vivaaerobus.com");
+        navigateTo(baseUrl);
+        String actualTitle = "Viva Aerobus";
+        assertThat(getDriver().getTitle()).contains(actualTitle);
+//        Screenshot.screenshot(getDriver());
+        log.info(" Test 1002 ----- SUCCESSFUL -------");
 
     }
 
